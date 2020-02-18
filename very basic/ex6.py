a1e113c4098387b595bc1f03d0179644de3a5dab
WanjikuMac <edwardscaroline32@gmail.com>
2020-02-18 11:42:23 +0300

    handle more strings

---
+++ very basic/ex6.py
@@ -0,0 +1,12 @@
+types_of_people = 10
+x = "There are {} types of people".format(types_of_people)
+print(x)
+hilarious = False
+joke_evaluation ="Isn't that joke so funny?! {}"
+
+print(joke_evaluation.format(hilarious))
+
+w = "This is the left side of...."
+e = "a string with a right side"
+
+print(w + e)
