877cc66b4f6ba7f16319a2a35fb6f6db0780cfe4
WanjikuMac <edwardscaroline32@gmail.com>
2020-02-19 19:01:00 +0300

    break the code

--- very basic/ex5.py
+++ very basic/ex5.py
@@ -14,7 +14,7 @@ print(f"Let's talk about {my_name}.")
 print("He's {} inches tall.".format(my_height))
 print("He's {} kgs heavy.".format(my_weight))
 print("Actually that's not too heavy")
-print("He's got {1} eyes and {0} hair.".format(my_hair, my_eyes))
+print("He's got {0} eyes and {1} hair.".format(my_hair, my_eyes))
 print("His teeth are usually {} depending on the food he takes".format(my_teeth))
 total = my_age + my_height + my_weight
 print("If I add {}, {}, and {} I get {}".format(my_age, my_height, my_weight, total))
