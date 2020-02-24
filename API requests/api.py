7de8dd189c5ffa7a9e27aa60e10d4cd70ff060f2
WanjikuMac <edwardscaroline32@gmail.com>
2020-02-24 14:38:52 +0300

    make a get request and print the json response

---
+++ API requests/api.py
@@ -0,0 +1,16 @@
+import requests
+import json
+#make a simple get request to an API that doen't exist 
+response1 = requests.get("http://api.open-notify.org/this-api-doesn't-exist")
+
+#make an api request to an existing API which doesn't require query parameters
+response = requests.get("http://api.open-notify.org/astros.json")
+#status code 200 is everything is fine, code 404 the server wasn't found
+print(response.status_code)
+print(response.json())
+
+def jprint(obj):
+    text = json.dumps(obj, sort_keys=True, indent=4)
+    print(text)
+
+jprint(response.json())
