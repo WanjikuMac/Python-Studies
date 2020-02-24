e87399e502c106c05daa2bc4ef945aba528fc6fb
WanjikuMac <edwardscaroline32@gmail.com>
2020-02-24 15:47:10 +0300

    loop and access the risetime data from the api

--- API requests/api.py
+++ API requests/api.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from datetime import datetime
 #make a simple get request to an API that doen't exist 
 response1 = requests.get("http://api.open-notify.org/this-api-doesn't-exist")
 
@@ -10,10 +11,11 @@ print(response.status_code)
 print(response.json())
 
 def jprint(obj):
+    #dumps prints a formatted string that makes it easier to understand the json output
     text = json.dumps(obj, sort_keys=True, indent=4)
     print(text)
 
-jprint(response.json())
+print(response.json())
 
 #make an API request with params
 parameters = {
@@ -21,6 +23,25 @@ parameters = {
     "lon": -74 
 }
 
+#This endpoint tells us the next times that the international space station will pass over a given location on the earth
 response = requests.get("http://api.open-notify.org/iss-pass.json", parameters)
 jprint(response.json())
 
+#dates printed in this are in epoch format 
+pass_times =response.json()['response']
+jprint(pass_times)
+
+risetimes = []
+for d in pass_times:
+    time = d['risetime']
+    risetimes.append(time)
+
+print(risetimes)
+
+#convert dates to timestamps and put them in a list
+times = []
+
+for rt in risetimes:
+    time =datetime.fromtimestamp(rt)
+    times.append(time)
+    print(time)
