af4b4ec8dd5f3d57b70e7cbefc2c44ed3137068b
WanjikuMac <edwardscaroline32@gmail.com>
2020-02-25 10:31:12 +0300

    handling inputs

---
+++ More than basic/ex11.py
@@ -0,0 +1,9 @@
+#dealing with inputs
+print("How old are you?", end=' ')
+age =input()
+print("Hoe tall are you?", end = ' ')
+height = input()
+print("How much do you weigh?", end = ' ')
+weight = input()
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy")
