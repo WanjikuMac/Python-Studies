81602f1e410ca96ecbfc29634658def1d18fc64b
WanjikuMac <edwardscaroline32@gmail.com>
2020-02-25 10:23:45 +0300

    work with single quotes instead of double

--- More than basic/ex10.py
+++ More than basic/ex10.py
@@ -2,11 +2,11 @@
 tabby_cat = "\tI'm tabbed in."
 persian_cat = "I'm split \non a line"
 backslach_cat = "I'm \\ a \\ cat."
-fat_cat = """
+fat_cat = '''
 I'll do a list:
 \t* Cat FOOD
 \t* Fishies\n\t* Catnip\n\t* Grass
-"""
+'''
 
 print(tabby_cat)
 print(persian_cat)
