045a03d4c1570b81d27ecbcad901d4450c50efb1
WanjikuMac <edwardscaroline32@gmail.com>
2020-02-25 10:21:39 +0300

    escape double quotes and single quotes

---
+++ More than basic/ex10.py
@@ -0,0 +1,14 @@
+#how to escape double quotes and single quotes and tab strings
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split \non a line"
+backslach_cat = "I'm \\ a \\ cat."
+fat_cat = """
+I'll do a list:
+\t* Cat FOOD
+\t* Fishies\n\t* Catnip\n\t* Grass
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backslach_cat)
+print(fat_cat)
