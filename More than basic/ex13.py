f6aa03ef8925080c3ff78a2090d053c94a557711
WanjikuMac <edwardscaroline32@gmail.com>
2020-02-26 10:53:59 +0300

    using the variable argv

---
+++ More than basic/ex13.py
@@ -0,0 +1,9 @@
+from sys import argv
+#read the WYSS section for how to run this
+#while running this script use this format python ex13.py, first, 2nd and 3rd
+script, first, second, third = argv
+
+print("The script is called:", script)
+print("Your first variable is:", first)
+print("Your second variable is:", second)
+print("Your third variable is:", third)
