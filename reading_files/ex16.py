49ad3d54e08d7e2aa4ab3ce166e71e8c169a3267
WanjikuMac <edwardscaroline32@gmail.com>
2020-02-28 18:43:18 +0300

    reading data then write it to a file

---
+++ reading_files/ex16.py
@@ -0,0 +1,26 @@
+from sys import argv
+
+script, filename = argv
+
+print(f"We're going to erase {filename}.")
+print("If you don't want that hit CTRL-C (^C).")
+print("If you want that, hit RETURN.")
+
+input("?")
+
+print("Opening the file....")
+target = open(filename, 'w')
+print("Truncating the file. Goodbye!")
+target.truncate()
+
+print("Now I'm going to ask you for three lines.")
+print("And write theses to the file.")
+target.write(input("line 1: "))
+target.write("\n")
+target.write(input("line 2: "))
+target.write("\n")
+target.write(input("line 3: "))
+target.write("\n")
+
+print("And finally, we close it")
+target.close()
