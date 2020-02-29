fde5716e10dbd245aafe00ac42f7e1de7a2cb4c3
WanjikuMac <edwardscaroline32@gmail.com>
2020-02-29 19:43:42 +0300

    rewrite the script

--- reading_files/ex17.py
+++ reading_files/ex17.py
@@ -5,19 +5,18 @@ script, from_file, to_file = argv
 
 print(f"Copying from {from_file} to {to_file}")
 
-#we could do these two on one line, hpw?
-
-indata = read.(open(from_file))
+#we could do these two on one line, how?
+indata = open(from_file).read()
 
 print(f"The input file is {len(indata)} bytes long")
 print(f"Does the output file esist? {exists(indata)}")
-print("Ready, hit RETURN to continue, CTRL-C to abort.")
-input()
+#print("Ready, hit RETURN to continue, CTRL-C to abort.")
+#input()
 
 out_file = open(to_file, "w")
-out_file.write(in_file)
+out_file.write(indata)
 
 print("Alright, all done.")
 
 out_file.close
-in_file.close
+#in_file.close
