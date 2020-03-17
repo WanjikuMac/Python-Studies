b16dcfb1bc72a546d0168cc081891ad61b08b64e
WanjikuMac <edwardscaroline32@gmail.com>
2020-03-17 07:54:40 +0300

    redo this with a function I can tell to replace 6

--- what_if/ex33.py
+++ what_if/ex33.py
@@ -1,10 +1,12 @@
+#numbers = []
+#print("Enter loop size")
+#val = input()
+#def enter_loop(val):
 i = 0 
 numbers = []
-
 while i < 6:
     print(f"At the top i is {i}")
     numbers.append(i)
-
     i = i + 1
     print("Numbers now: ", numbers)
     print(f"At the bottom i is {i}")
