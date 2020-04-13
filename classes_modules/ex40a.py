1aa6120db474a6abce804573efb77cddfca3dc44
WanjikuMac <edwardscaroline32@gmail.com>
2020-04-13 16:01:48 +0300

    module and dict style to get things

---
+++ classes_modules/ex40a.py
@@ -0,0 +1,12 @@
+#module and dict sytle to get things from things
+import mystuff
+mystuff.apple()
+print(mystuff.tangerine)
+
+
+mystuff.apple() #get apple from the module
+mystuff.tangerine #same thing it's just a variable
+
+mystuff = {'apple': "I AM APPLES!"}
+mystuff['apple'] #get apple form dict
+print(mystuff['apple'])
