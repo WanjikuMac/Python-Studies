73b830a779d007e5d63fc75429713037e8cc7a0f
WanjikuMac <edwardscaroline32@gmail.com>
2020-04-13 16:03:28 +0300

    sample code that uses class to access things

---
+++ classes_modules/ex40.py
@@ -0,0 +1,21 @@
+class Song(object):
+
+    def __init__(self, lyrics):
+        self.lyrics = lyrics
+    
+    def sing_me_a_song(self):
+        for line in self.lyrics:
+            print(line)
+
+#instantiate your class with data
+Z = ["Happy birthday to you",
+    "I don't want to get sued",
+    "So I'll stop right there"]
+happy_bday = Song(Z)
+
+#instantiate your class with data
+bulls_on_parade = Song(["They rally around the family",
+                        "with pockets full of shells"])
+
+happy_bday.sing_me_a_song()
+bulls_on_parade.sing_me_a_song()
