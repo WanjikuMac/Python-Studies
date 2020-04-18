63fb6f6c9e0f6bd5a8f72b41f4b9373e18b9a3c2
WanjikuMac <edwardscaroline32@gmail.com>
2020-04-18 11:30:56 +0300

    add multiples of 3 and 5 plus this should be in erlang folder

---
+++ multiples_of_3_and_5.erl
@@ -0,0 +1,42 @@
+%sum of numbers of mulitples of 3 and 5 below 1000
+
+-module(multiples_of_3_and_5).
+ -export([sum/1, sum_multiples/1]).
+
+
+
+sum_multiples(Number) ->
+    V = fun(X) 
+                when (X rem 3 == 0) or (X rem 5 == 0) -> X;
+            (X)
+               when (X rem 3 =/= 0) or (X rem 5 =/= 0) -> 0
+        end,
+       Z =  lists:map(V, Number),
+        sum(Z).
+
+sum(L) -> sum(L, 0).
+
+sum([], Sum) -> Sum;
+sum([H |T], Sum) -> sum(T, H+Sum).
+
+
+% sum(L) -> sum(L,0).
+ 
+% sum([], Sum) -> Sum;
+% sum([H|T], Sum) -> sum(T, H+Sum).
+    % Target = Number -1,
+    % Sum = 0,
+    % case (Target rem 3 == 0) or (Target rem 5 == 0) of 
+    %     true -> Sum + Target    
+    % end.
+
+
+%    Totals = fun(Value) when Target < Number ->
+%         Sum = 0,
+%          case Target rem 3 == 0 or (Target rem 5 == 0)  of 
+%              true -> Sum + Target;
+%             false -> io:format("isnot working boo")
+%         end
+%     end,
+%     Totals.
+
